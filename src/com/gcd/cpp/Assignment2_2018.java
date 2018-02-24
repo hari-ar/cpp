f0a65608e70c996cb55fd00f620d0199de19bf6c
larhari <aahuyarakshaka.harilakshminarasimhan@student.griffith.ie>
2018-02-24 01:11:09 +0000

    question1 complete..

--- src/com/gcd/cpp/Assignment2_2018.java
+++ src/com/gcd/cpp/Assignment2_2018.java
@@ -9,27 +9,107 @@ package com.gcd.cpp;
  */
 // Code for Question 1 here =======================================
 
+class ZeroFinder extends Thread{
+
+    private int[] hayStackArray;
+    private int lowerBound, upperBound;
+    private int threadId;
+    private Helper helperInstance;
+
+
+    //Constructor
+    ZeroFinder(int[] hayStackArray, int lowerBound, int upperBuond, int threadId, Helper helperInstance) {
+        this.hayStackArray = hayStackArray;
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBuond;
+        this.threadId = threadId;
+        this.helperInstance = helperInstance;
+    }
+
+    @Override
+    public void run() {
+        for(int index =lowerBound; index<upperBound; index++){
+            //System.out.println("thread "+ threadId + " index "+ index + " Value : "+hayStackArray[index]); //Enabled for debuggin.
+
+            //Check if a zero is already found at a lower index
+            if(!helperInstance.isFound() || (helperInstance.isFound() && index<helperInstance.getIndexPosition()))
+            {
+                if(hayStackArray[index]==0){
+                    helperInstance.setAllValues(threadId,index);
+                }
+            }
+            else{
+                //Do not go further..!! Added for optimization.
+                break;
+            }
+        }
+    }
+}
+
+//POJO helper
+class Helper
+{
+    private boolean isFound = false;
+    private int indexPosition = -1;
+
+    int getIndexPosition() {
+        return indexPosition;
+    }
+
+    boolean isFound() {
+        return isFound;
+    }
+
+    void setAllValues(int foundByThreadId, int indexPosition) {
+        if(!isFound){
+            isFound = true;
+            this.indexPosition = indexPosition;
+        }
+        if(this.indexPosition>indexPosition)
+        {
+            isFound = true;
+            this.indexPosition = indexPosition;
+        }
+    }
+}
+
+//============================================================================//
+//
+// Main Method For the Assignment. Above classes are exclusive to question 1.
+//
+//============================================================================//
+
 public class Assignment2_2018 {
 
     public static void main(String args[]){
 
-        int[] aVeryLargeArray = new int[10000000];
+//        int[] aVeryLargeArray = new int[10]; Enabled for debugging. Comment the line below.
+        System.out.println("\n================Begininng of Question1================");
 
+        //Initial Declaration//
+        int[] aVeryLargeArray = new int[1000000];
         int numberOfThreads = Runtime.getRuntime().availableProcessors();
         int[] indexPointer = new int[numberOfThreads+1];
         Helper helperInstance = new Helper();
         Thread workers[] = new ZeroFinder[numberOfThreads];
+
+        //Initializing array with random values
         for(int i=0;i<aVeryLargeArray.length;i++){
-            aVeryLargeArray[i] = (int) (Math.random()*1000);
+            aVeryLargeArray[i] = (int) (Math.random()*50000);
         }
+
+        //Code to distribute load between threads
         for(int i=0;i<=numberOfThreads;i++){
             indexPointer[i] = (i*aVeryLargeArray.length)/numberOfThreads;
-            //System.out.println(indexPointer[i]);
         }
+
+        //Starting the threads.
         for(int i=0;i<numberOfThreads;i++){
             workers[i] = new ZeroFinder(aVeryLargeArray, indexPointer[i], indexPointer[i+1], i+1, helperInstance);
             workers[i].start();
         }
+
+        //Code to let the threads join back.
         for(int i=0;i<numberOfThreads;i++){
             try {
                 workers[i].join();
@@ -37,107 +117,38 @@ public class Assignment2_2018 {
                 //Do Nothing
             }
         }
+
+        //Getting Index of first 0 occurance.
         if(helperInstance.isFound())
-        {
-            System.out.println("Value is : ");
-            System.out.println(helperInstance.getIndexPosition());
-
-        }
+            System.out.println("Zero is found in Array. Index Value of first occurance is : "+helperInstance.getIndexPosition());
         else
-        {
-            System.out.println("Not found");
-        }
+            System.out.println("Not found in the Array.!");
 
+
+        System.out.println("===================End of Question1===================\n");
+        System.out.println("================Begininng of Question2================");
+        //Enabled for debugging.
         //System.out.println("Array is :");
         /*for (int value : aVeryLargeArray
                 ) {
             System.out.print(" "+value);
         }*/
     }
+//============================================================================//
+//
+// End of all Question 1 info.
+//
+//============================================================================//
+
+    //End Question 1 =======================================================
+
 
 }
 
 
-class ZeroFinder extends Thread{
 
-    int[] hayStackArray;
-    int lowerBound, upperBound;
-    int threadId;
-    Helper helperInstance;
 
-    public ZeroFinder(int[] hayStackArray, int lowerBound, int upperBuond, int threadId, Helper helperInstance) {
-        this.hayStackArray = hayStackArray;
-        this.lowerBound = lowerBound;
-        this.upperBound = upperBuond;
-        this.threadId = threadId;
-        this.helperInstance = helperInstance;
-        //System.out.println(lowerBound+" .. "+ upperBuond+" .. "+ threadId);
-    }
 
-    @Override
-    public void run() {
-        for(int index =lowerBound; index<upperBound; index++){
-            //System.out.println("thread "+ threadId + " index "+ index + " Value : "+hayStackArray[index]);
-            if(!helperInstance.isFound() || (helperInstance.isFound() && helperInstance.getFoundByThreadId()>threadId))
-            {
-                if(hayStackArray[index]==0){
-                    helperInstance.setAllValues(threadId,index);
-                }
-            }
-            else{
-                break;
-            }
-        }
-    }
-}
-
-class Helper
-{
-    private boolean isFound = false;
-    private int foundByThreadId = -1;
-    private int indexPosition = -1;
-
-    public int getIndexPosition() {
-        return indexPosition;
-    }
-
-    public void setIndexPosition(int indexPosition) {
-        this.indexPosition = indexPosition;
-    }
-
-    public boolean isFound() {
-        return isFound;
-    }
-
-    public void setFound(boolean found) {
-        isFound = found;
-    }
-
-    public int getFoundByThreadId() {
-        return foundByThreadId;
-    }
-
-    public void setFoundByThreadId(int foundByThreadId) {
-        this.foundByThreadId = foundByThreadId;
-    }
-
-    public void setAllValues(int foundByThreadId, int indexPosition) {
-        if(this.foundByThreadId<0){
-            setFound(true);
-            setIndexPosition(indexPosition);
-            setFoundByThreadId(foundByThreadId);
-        }
-
-        if(this.foundByThreadId>foundByThreadId)
-        {
-            setFound(true);
-            setIndexPosition(indexPosition);
-            setFoundByThreadId(foundByThreadId);
-        }
-    }
-}
-
-//End Question 1 =======================================================
 
 //Code for Sieve of Erathostenes
 
